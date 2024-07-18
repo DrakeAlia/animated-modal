8b5cc0eae7e45be40565f54430e851f6927ea818
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-07-17 20:39:13 -0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { AnimatedModalDemo } from "@/components/animated-modal-demo";
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <AnimatedModalDemo />
+    </main>
+  );
+}
